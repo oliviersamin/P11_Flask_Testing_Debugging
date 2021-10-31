3c9f6b6d664d19360fd83e4d15bef9567cd829c0
Olivier SAMIN <oliviersamin@gmail.com>
2021-10-31 22:07:25 +0000

    new feature: 1.performance test updated to book a competition in the future.

--- tests/performance/all_performance_tests.py
+++ tests/performance/all_performance_tests.py
@@ -1,10 +1,43 @@
 from locust import HttpUser, task
 import server
+import json
+
 
 
 class TestPerformance(HttpUser):
     """ This test check the pergormance to reach each page of the website.
      Therefore each method correspond to one page of the website"""
+
+    futur_competition = {"name": "Test_competition_in_future", "date": "2022-03-27 10:00:00",
+                     "numberOfPlaces": "200"}
+
+    def __load_competitions(self):
+        with open('competitions.json') as comps:
+            competitions = json.load(comps)['competitions']
+            return competitions
+
+    def __setup_competition(self):
+        compets = self.__load_competitions()
+        if compets[-1]['name'] != self.futur_competition['name']:
+            compets.append(self.futur_competition)
+            compets = {'competitions': compets}
+            with open('competitions.json', 'w') as comps:
+                json.dump(compets, comps)
+
+    def __tear_down_competitions(self):
+        compet = self.__load_competitions()
+        compet = compet[:2]
+        compet = {'competitions': compet}
+        with open('competitions.json', 'w') as comps:
+            json.dump(compet, comps)
+
+    def on_start(self):
+        self.__setup_competition()
+        print("/n###### ON START #########/n", flush=True)
+
+    def on_stop(self):
+        self.__tear_down_competitions()
+        print("/n###### ON STOP #########/n", flush=True)
 
     @task(6)
     def access_index(self):
@@ -17,12 +50,15 @@ class TestPerformance(HttpUser):
 
     @task(6)
     def access_book(self):
-        url = 'http://127.0.0.1:5000/book/' + server.competitions[0]['name'] + '/' + server.clubs[0]['name']
+        competitions = self.__load_competitions()
+        url = 'http://127.0.0.1:5000/book/' + competitions[-1]['name'] + '/' + server.clubs[0]['name']
+        print("/n###### ACCES BOOK #########/n", url, flush=True)
         self.client.get(url)
 
     @task(6)
     def access_purchase_places(self):
-        data = {'competition': server.competitions[0]['name'], 'club': server.clubs[0]['name'],
+        competitions = self.__load_competitions()
+        data = {'competition': competitions[-1]['name'], 'club': server.clubs[0]['name'],
                 'places': "1"}
         self.client.post('http://127.0.0.1:5000/purchasePlaces', data)
 
